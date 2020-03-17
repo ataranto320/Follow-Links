11982c3288fa8606531823900e5ac232b5bba7ed
Austin T <austin.taranto@gmail.com>
2020-03-17 19:50:11 -0400

    highlight link function

--- javascript.js
+++ javascript.js
@@ -4,3 +4,10 @@ const triggers = document.querySelectorAll("a");
 const highlight = document.createElement("span");
 highlight.classList.add("highlight");
 document.body.append(highlight);
+
+function highlightLink() {
+    // console.log("highlight");
+    console.log(this);
+}
+
+triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
